26aef83f4451d4188b17a002ffa94a0e56ca0fa8
infinitas <infinitas@infinitass-Mac.local>
2013-03-16 17:01:04 -0700

    Initial Commit

---
+++ COMP2601ObjectiveCThreadWithReactor/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  COMP2601ObjectiveCThreadWithReactor
+//
+//  Created by infinitas on 2013-03-16.
+//  Copyright (c) 2013 John Cooper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
